674c174e01afea67a3892cfbb9bae49c7503d81c
kydtrai11 <138143346+kydtrai11@users.noreply.github.com>
2025-08-23 20:16:49 +0700

    asd

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,14 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '5001',
-        pathname: '/uploads/**',
+        protocol: "https",
+        hostname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'example.com',
-        port: '', // để trống vì hầu hết prod chạy mặc định 443
-        pathname: '/uploads/**',
-      },
-    ],
+        protocol: "http",
+        hostname: "**",
+      }
+    ]
   },
   eslint: {
     ignoreDuringBuilds: true, // tránh fail khi deploy do warnings
