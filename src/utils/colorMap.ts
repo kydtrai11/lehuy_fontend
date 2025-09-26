0788615d90ad47488a312a6e4ab4af313039568d
kydtrai11 <138143346+kydtrai11@users.noreply.github.com>
2025-09-26 20:59:39 +0700

    asd

---
+++ src/utils/colorMap.ts
@@ -0,0 +1,18 @@
+// utils/colorMap.ts
+export const colorMap: Record<string, string> = {
+  Đen: "#000000",
+  Trắng: "#FFFFFF",
+  Đỏ: "#FF0000",
+  Xanh: "#007BFF",       // xanh dương
+  "Xanh lá": "#28A745",
+  Vàng: "#FFD700",
+  Hồng: "#FF69B4",
+  Tím: "#800080",
+  Nâu: "#8B4513",
+  Xám: "#808080",
+  Kem: "#FFFDD0",
+  Be: "#F5F5DC",
+  Navy: "#001F54",
+  Cam: "#FF7F00",
+  "Đỏ đô": "#8B0000",
+};
